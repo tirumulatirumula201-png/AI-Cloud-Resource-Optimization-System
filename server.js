60ca62a0e5808fa64700f170ff0bfaa0c3b21a12
Tirumula <tirumulatirumula201@gmail.com>
2026-06-02 14:28:23 +0530

    fix gmail auth final

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
         user: "tirumulatirumula201@gmail.com",
-        pass: "psmqgzqnhgxyramo"   // your Gmail App Password
+        pass: "pnbcqpotvudbeicl"   // your Gmail App Password
     }
 });
 
